b5cc5746ac7af89c0dcb13e817fc93e1719db1de
shaniamehta01 <shaniamehta01@gmail.com>
2026-03-27 19:22:52 +0530

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
@@ -7,18 +6,20 @@ import plotly.express as px
 
 st.title("Clustering")
 
-file = st.file_uploader("Upload CSV", key="cluster")
+# LOAD DATA
+df = pd.read_csv("muse_dataset.csv")
 
-if file:
-    df = pd.read_csv(file)
-    df_enc = df.copy()
+# Encode
+df_enc = df.copy()
+le = LabelEncoder()
 
-    le = LabelEncoder()
-    for col in df_enc.select_dtypes(include='object').columns:
-        df_enc[col] = le.fit_transform(df_enc[col].astype(str))
+for col in df_enc.select_dtypes(include='object').columns:
+    df_enc[col] = le.fit_transform(df_enc[col].astype(str))
 
-    kmeans = KMeans(n_clusters=3, random_state=42)
-    df_enc['Cluster'] = kmeans.fit_predict(df_enc)
+# KMeans
+kmeans = KMeans(n_clusters=3, random_state=42)
+df_enc['Cluster'] = kmeans.fit_predict(df_enc)
 
-    fig = px.scatter(df_enc, x="Monthly_Spend", y="Outfit_Budget", color="Cluster")
-    st.plotly_chart(fig)
+# Plot
+fig = px.scatter(df_enc, x="Monthly_Spend", y="Outfit_Budget", color="Cluster")
+st.plotly_chart(fig)
