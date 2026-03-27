2a38b8b68fc4d2f7e4d1c8e9902a70f48254ace8
shaniamehta01 <shaniamehta01@gmail.com>
2026-03-27 19:05:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-
 import streamlit as st
 
 st.set_page_config(page_title="Muse AI App", layout="wide")
 
 st.title("Muse AI Fashion Analytics App")
 
-st.markdown("Use sidebar to navigate across pages.")
+# Sidebar navigation
+page = st.sidebar.radio("Go to", [
+    "Executive Summary",
+    "Descriptive Analysis",
+    "Clustering",
+    "Association Rules",
+    "Prediction Models",
+    "Prescriptive Actions",
+    "New Customer Scorer"
+])
+
+if page == "Executive Summary":
+    import descriptive
+elif page == "Descriptive Analysis":
+    import descriptive
+elif page == "Clustering":
+    import clustering
+elif page == "Association Rules":
+    import association
+elif page == "Prediction Models":
+    import prediction
+elif page == "Prescriptive Actions":
+    import prescriptive
+elif page == "New Customer Scorer":
+    import new_user
